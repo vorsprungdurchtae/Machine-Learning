28571adde4c9832d6e3edbadbf18a60a39be0073
Tae Keun Jeong <xorms90@gmail.com>
2021-10-08 21:13:57 +0200

    knn complete

--- exercise-01/exercise-01-solutions/q5_knn_python/knn.py
+++ exercise-01/exercise-01-solutions/q5_knn_python/knn.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 def knn(samples, k):
     # compute density estimation from samples with KNN
@@ -9,21 +9,29 @@ def knn(samples, k):
     # Output
     #  estDensity : estimated density in the range of [-5, 5]
 
-    #####Start Subtask 5b#####
+    #####Insert your code here for subtask 5b#####
     # Compute the number of the samples created
-    N = len(samples)
-
-    # Create a linearly spaced vector
     pos = np.arange(-5, 5.0, 0.1)
 
-    # Sort the distances so that we can choose the k-th point
-    dists = np.sort(np.abs(pos[np.newaxis, :] - samples[:, np.newaxis]), axis=0)
+    if len(samples.shape) > 1:
 
-    # Estimate the probability density using the k-NN density estimation
-    res = (k / (2 * N)) / dists[k - 1, :]
+        D = samples.shape[1]
 
-    # Form the output variable
-    estDensity = np.stack((pos, res), axis=1)
+    else:
 
-    #####End Subtask#####
+        D = 1
+
+    N = samples.shape[0]
+
+    #the distances to the data point from the interval [-5 5]
+    #to the samples
+    dist = np.abs(pos[np.newaxis, :] - samples[:, np.newaxis])
+
+    #sort the distances
+    #extract k-1 th element from the sorted diatances
+    sorted_dist = np.sort(dist, axis = 0)[k-1, :]
+    densities = k / (sorted_dist * 2 * N)
+
+    estDensity = np.column_stack((pos, densities))
+
     return estDensity
